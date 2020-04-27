b71d2954aab8b49b8f892b42bbf2a4569816c989
Ngube-hermann <hngube101@gmail.com>
2020-04-27 06:24:19 +0100

    1. Provide the number of rows and columns in this dataset.

---
+++ Data_code_python.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import ALookAtTheData as t
+from IPython import display
+%matplotlib inline
+
+df = pd.read_csv('./survey_results_public.csv')
+df.head()
+
+------//////////////-------------------////
+
+
